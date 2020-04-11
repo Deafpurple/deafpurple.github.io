9f42f9667aaefd58b4270e7edc0c68dc59907763
Deafpurple <63457115+Deafpurple@users.noreply.github.com>
2020-04-11 11:37:31 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 function main(){
-	alert ("Main!");
+	VK.init(onSuccess, onFail, "5.103");
+	//alert ("Main!");
+}
+
+function onSuccess(){
+	alert ("Success!");
+}
+
+function onFail(){
+	alert ("Fail!");
 }
